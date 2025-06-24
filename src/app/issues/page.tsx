23960adb25eeb748be73e937894903533688626c
Aditya Purohit <aditya.s.purohit@gmail.com>
2025-06-24 14:06:12 -0400

    build the new issue page

--- src/app/issues/page.tsx
+++ src/app/issues/page.tsx
@@ -1,10 +1,13 @@
 import React from "react";
+import Link from "next/link";
 import { Button } from "@radix-ui/themes";
 
 function IssuesPage() {
   return (
     <div>
-      <Button>New issue</Button>
+      <Button>
+        <Link href="/issues/new">New issue</Link>
+      </Button>
     </div>
   );
 }
